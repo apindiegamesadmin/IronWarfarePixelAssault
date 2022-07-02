b1c332b45c508a04ec12b08b850619beac609d6f
apindiegames-admin <apindiegames@gmail.com>
2022-07-02 22:25:01 +1000

    Updated files with .gitignore. Library files and other large unnecessary files will not be updated.

---
+++ Assets/Scripts/TankMover.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TankMover : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+
+    public TankMovementData movementData;
+
+    private Vector2 movementVector;
+    private float currentSpeed = 0;
+    private float currentForewardDirection = 1;
+
+    public UnityEvent<float> OnSpeedChange = new UnityEvent<float>();
+
+    private void Awake()
+    {
+        rb2d = GetComponentInParent<Rigidbody2D>();
+    }
+
+    public void Move(Vector2 movementVector)
+    {
+        this.movementVector = movementVector;
+        CalculateSpeed(movementVector);
+        OnSpeedChange?.Invoke(this.movementVector.magnitude);
+        if (movementVector.y > 0)
+        {
+            if (currentForewardDirection == -1)
+                currentSpeed = 0;
+            currentForewardDirection = 1;
+        }  
+        else if (movementVector.y < 0)
+        {
+            if (currentForewardDirection == 1)
+                currentSpeed = 0;
+            currentForewardDirection = -1;
+        }
+            
+    }
+
+    private void CalculateSpeed(Vector2 movementVector)
+    {
+        if (Mathf.Abs(movementVector.y) > 0)
+        {
+            currentSpeed += movementData.acceleration * Time.deltaTime;
+        }
+        else
+        {
+            currentSpeed -= movementData.deacceleration * Time.deltaTime;
+        }
+        currentSpeed = Mathf.Clamp(currentSpeed, 0, movementData.maxSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.velocity = (Vector2)transform.up * currentSpeed * currentForewardDirection * Time.fixedDeltaTime;
+        rb2d.MoveRotation(transform.rotation * Quaternion.Euler(0, 0, -movementVector.x * movementData.rotationSpeed * Time.fixedDeltaTime));
+    }
+}
