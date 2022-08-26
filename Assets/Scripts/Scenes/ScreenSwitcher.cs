216e4b9a021790591eedd8a7a3f79aaf3bbb9f5f
naingkokolin <naingkokolin007@gmail.com>
2022-08-26 12:08:53 +0630

    1. Added tank selection functionality in main menu scene

--- Assets/Scripts/Scenes/ScreenSwitcher.cs
+++ Assets/Scripts/Scenes/ScreenSwitcher.cs
@@ -9,9 +9,19 @@ public class ScreenSwitcher : MonoBehaviour
     public GameObject mainMenuPanel;
     public GameObject settingsMenuPanel;
     public GameObject loadingScreen;
+    public GameObject skinSelection;
+
+    bool isTankSelected;
+
+    private void Start()
+    {
+        skinSelection.SetActive(false);
+        isTankSelected = false;
+    }
 
     IEnumerator FakeLoadingScreen(int sceneIndex)
     {
+        skinSelection.SetActive(false);
         loadingScreen.SetActive(true);//Enable Fake Loading Screen
         yield return new WaitForSeconds(2f);//Delay Before LoadScene
         SceneManager.LoadScene(sceneIndex);
@@ -31,6 +41,20 @@ public class ScreenSwitcher : MonoBehaviour
 
     public void LoadSceneIndex(int index)
     {
-        StartCoroutine(FakeLoadingScreen(index));
+        if (isTankSelected)
+        {
+            StartCoroutine(FakeLoadingScreen(index));
+        }
+        else
+        {
+            OnClick_Play();
+        }
+
+    }
+
+    public void OnClick_Play()
+    {
+        skinSelection.SetActive(true);
+        isTankSelected = true;
     }
 }
