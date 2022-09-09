cec2587c13dd92a93c172c267848e78f0779a707
WaiYan440 <waiyan4403@gmail.com>
2022-09-09 18:16:48 +0630

    Made a destroyable crate functionality

--- Assets/Scripts/Soldier/BodyPartsSpawner.cs
+++ Assets/Scripts/Soldier/BodyPartsSpawner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BodyPartsSpawner : MonoBehaviour
 {
+    [SerializeField] float randomXValue = 1f;
     [SerializeField] GameObject[] bodyPartsPrefabs;
 
     public void SpawnBodyParts(Vector2 dir,Vector2 pos)
@@ -13,10 +14,10 @@ public class BodyPartsSpawner : MonoBehaviour
             GameObject bodyPart = Instantiate(bodyPartsPrefabs[i],transform.position,Quaternion.identity);
             Rigidbody2D rb = bodyPart.GetComponent<Rigidbody2D>();
 
-            float randomValue = Random.Range(0f, 1f);
-            rb.AddForceAtPosition((new Vector2(dir.x - randomValue,dir.y)) * 100f, pos);
+            float randomValue = Random.Range(0f, randomXValue);
+            rb.AddForceAtPosition((new Vector2(dir.x - randomValue,dir.y - randomValue)) * 100f, pos);
 
-            Destroy(bodyPart, 2f);
+            Destroy(bodyPart, 1f);
         }
     }
 }
