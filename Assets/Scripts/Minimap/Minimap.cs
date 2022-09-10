435d4afe9403c75f3db82810d72d91b76c709935
naingkokolin <naingkokolin007@gmail.com>
2022-09-10 21:45:42 +0630

    1. Updating minimap ui & functionality to show the location of enemies to the player 2. Made some ui changes for minimap preview in mission 1 3. Added some sprites & textures 4. Added minimapicon red dot for enemy small tanks & green dot for player

--- Assets/Scripts/Minimap/Minimap.cs
+++ Assets/Scripts/Minimap/Minimap.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class Minimap : MonoBehaviour
 {
-    Transform player;
-    //Vector3 pos;
+    Transform point;
+    Transform _player;
+    Transform _target;
+    float minimapSize = 5f;
 
-    void Start()
+    void Awake()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        // transform.position = new Vector3(-12.1417198f, -13.2733755f, 0.110948563f);
+        _target = GameObject.FindGameObjectWithTag("Enemy").transform;
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     public void LateUpdate()
     {
-        Vector3 newPosition = player.position;
+        Vector3 newPosition = _player.position;
         newPosition.z = transform.position.z;
         transform.position = newPosition;
+
+        // Center of Minimap
+        Vector2 _centerPosition = transform.localPosition;
+
+        // Distance from the gameobject to minimap
+        float distance = Vector2.Distance(_target.position, _centerPosition);
+
+        // If the Distance is less than MinimapSize, it is within the Minimap view and we don't need to do anything
+        // But if the Distance is greater than the MinimapSize, then do this
+        if (distance > minimapSize)
+        {
+            // Target - Minimap
+            Vector2 fromOriginToObject = (Vector2)_target.position - _centerPosition;
+
+            // Multiply by MinimapSize and Divide by Distance
+            fromOriginToObject *= minimapSize / distance;
+
+            // Minimap + above calculation
+            transform.position = _centerPosition + fromOriginToObject;
+        }
     }
 }
