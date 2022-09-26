b59be32de1f29c595e1821555f63dab00adbf1f7
WaiYan440 <waiyan4403@gmail.com>
2022-09-26 14:59:53 +0630

    1. Remade a new map for level selection 2. Adjusted positions and animations of the missions 3. Added mission unlock and lock functions for mission 3,4,5

--- Assets/Scripts/Missions/MissionManager.cs
+++ Assets/Scripts/Missions/MissionManager.cs
@@ -8,6 +8,12 @@ public class MissionManager : MonoBehaviour
     public int currentMission;
     [SerializeField] GameObject mission2Locked;
     [SerializeField] GameObject mission2UnLocked;
+    [SerializeField] GameObject mission3Locked;
+    [SerializeField] GameObject mission3UnLocked;
+    [SerializeField] GameObject mission4Locked;
+    [SerializeField] GameObject mission4UnLocked;
+    [SerializeField] GameObject mission5Locked;
+    [SerializeField] GameObject mission5UnLocked;
 
     void Start()
     {
@@ -25,6 +31,18 @@ public class MissionManager : MonoBehaviour
                 mission2Locked.SetActive(true); // Show Locked UI
                 mission2UnLocked.SetActive(false); // Hide Unlocked UI
 
+                // Mission 3
+                mission3Locked.SetActive(true); // Show Locked UI
+                mission3UnLocked.SetActive(false); // Hide Unlocked UI
+
+                // Mission 4
+                mission4Locked.SetActive(true); // Show Locked UI
+                mission5UnLocked.SetActive(false); // Hide Unlocked UI
+
+                // Mission 5
+                mission5Locked.SetActive(true); // Show Locked UI
+                mission5UnLocked.SetActive(false); // Hide Unlocked UI
+
                 break;
 
             case 1: //If the player completed Mission 1, unlock Mission 2
@@ -33,12 +51,18 @@ public class MissionManager : MonoBehaviour
                 break;
 
             case 2://If the player completed Mission 2, unlock Mission 3
+                mission3Locked.SetActive(false); // Hide Locked UI
+                mission3UnLocked.SetActive(true); // Show Unlocked UI
                 break;
 
             case 3://If the player completed Mission 3, unlock Mission 4
+                mission4Locked.SetActive(false); // Hide Locked UI
+                mission4UnLocked.SetActive(true); // Show Unlocked UI
                 break;
 
             case 4://If the player completed Mission 4, unlock Mission 5
+                mission5Locked.SetActive(false); // Hide Locked UI
+                mission5UnLocked.SetActive(true); // Show Unlocked UI
                 break;
         }
     }
