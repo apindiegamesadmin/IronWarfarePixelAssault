e27b8f7a23899d3683db169fc4b4b677d2f9a1f6
naingkokolin <naingkokolin007@gmail.com>
2022-09-28 00:21:12 +0630

    1. Made red dot for mission 1 boss

---
+++ Assets/Scripts/Minimap/IconAnimation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconAnimation : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    private void Awake()
+    {
+        // animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayAnimation()
+    {
+
+    }
+}
