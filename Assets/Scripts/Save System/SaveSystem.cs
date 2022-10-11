f205d4f9fa42ae368715fc956769d459eb9a847d
WaiYan440 <waiyan4403@gmail.com>
2022-10-11 15:13:56 +0630

    Adjusted Save System for android

--- Assets/Scripts/Save System/SaveSystem.cs
+++ Assets/Scripts/Save System/SaveSystem.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public static class SaveSystem
 {
     private static readonly string SAVE_FOLDER = Application.dataPath + "Resources/Saves/";
+    private static readonly string ANDROID_SAVE_FOLDER = Application.persistentDataPath + "Resources/Saves/";
     public static void Init()
     {
+#if UNITY_EDITOR
         if (!Directory.Exists(SAVE_FOLDER))
         {
-
             Directory.CreateDirectory(SAVE_FOLDER);
         }
+#elif UNITY_ANDROID
+        if (!Directory.Exists(ANDROID_SAVE_FOLDER))
+        {
+            Directory.CreateDirectory(ANDROID_SAVE_FOLDER);
+        }
+#endif
     }
     public static void SavePlayerData(Player player)
     {
         Init();
         BinaryFormatter formatter = new BinaryFormatter();
+#if UNITY_EDITOR
         string path = SAVE_FOLDER + "/player.dat";
+#elif UNITY_ANDROID
+        string path = ANDROID_SAVE_FOLDER + "/player.dat";
+#endif
         FileStream stream = new FileStream(path, FileMode.Create);
 
         PlayerData data = new PlayerData(player);
@@ -28,7 +39,11 @@ public static class SaveSystem
 
     public static PlayerData loadPlayerData()
     {
+#if UNITY_EDITOR
         string path = SAVE_FOLDER + "/player.dat";
+#elif UNITY_ANDROID
+        string path = ANDROID_SAVE_FOLDER + "/player.dat";
+#endif
 
         if (File.Exists(path))
         {
