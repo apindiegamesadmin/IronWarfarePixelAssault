e680ccc7b55df39885711cfba9e71813e7593cf3
naingkokolin <naingkokolin007@gmail.com>
2022-12-25 22:04:57 +0630

    1. Made capture screen shot prefab 2. Added capture screen shot prefab in mission 1, 2, 3 & 4 to take screenshot for google play store

--- Assets/Scripts/Promo Scene/CaptureScreenShoot.cs
+++ Assets/Scripts/Promo Scene/CaptureScreenShoot.cs
@@ -9,7 +9,7 @@ public class CaptureScreenShoot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        index = 0;
     }
 
     // Update is called once per frame
